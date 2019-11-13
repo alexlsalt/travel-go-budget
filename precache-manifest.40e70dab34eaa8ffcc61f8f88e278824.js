ba66df44d4b643d44a8153a54a32184c6a4d62ac
Alex Morton <alexmorton@alexs-macbook-pro.home>
2019-11-13 16:06:43 +0100

    Updates

---
+++ precache-manifest.40e70dab34eaa8ffcc61f8f88e278824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6637a36291ee863f8293dfd1836cea04",
+    "url": "/travel-go-budget/index.html"
+  },
+  {
+    "revision": "aa1b64648aa3bc702491",
+    "url": "/travel-go-budget/static/css/main.27ea16b0.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/travel-go-budget/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "aa1b64648aa3bc702491",
+    "url": "/travel-go-budget/static/js/main.ef696323.chunk.js"
+  },
+  {
+    "revision": "bc4cef368bfc2a2219a3",
+    "url": "/travel-go-budget/static/js/runtime~main.8d9518a4.js"
+  },
+  {
+    "revision": "10766ba19f3d625aef35a2220cfa72b8",
+    "url": "/travel-go-budget/static/media/travel-min.10766ba1.jpg"
+  }
+]);
